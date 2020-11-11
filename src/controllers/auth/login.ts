b58c993894b883de45e6877d7fc0fd5bd598bc4e
sueryong <csr0604@hanmail.net>
2020-11-11 20:40:33 +0900

    [로그인 api 구현] typeorm을 이용한 로그인 api 구현

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,5 +1,22 @@
+import { RSA_NO_PADDING } from "constants";
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import { User } from "../../db/entities/User";
 
-export default async (req: Request, res: Response) => {};
+export default async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await getRepository(User)
+    .createQueryBuilder("user")
+    .where("user.email= :email", { email })
+    .getOne();
+
+  if (user === undefined) {
+    return res.status(401).send("회원가입 해야함");
+  } else {
+    if (user.email === email && user.password === password) {
+      return res.status(200).send("로그인 완료");
+    }
+  }
+  return res.status(400);
+};
