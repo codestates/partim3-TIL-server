cb7a56db9ad32a0bac86e10a11a33c0231bde565
sueryong <csr0604@hanmail.net>
2020-11-15 17:18:26 +0900

    [로그아웃 api 구현] 데이터베이스 토큰 삭제

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from "express";
-import { getConnection, IsNull } from "typeorm";
+import { getConnection } from "typeorm";
 import { User } from "../../db/entities/User";
 
 export default async (req: Request, res: Response) => {
-  const token = req.header("Authorization");
-
-  res.removeHeader("Authorization");
+  const { id } = req.body;
 
   await getConnection()
     .createQueryBuilder()
     .update(User)
     .set({ token: undefined })
-    .where("token = :token", { token })
-    .execute();
+    .where("id = :id", { id })
+    .execute()
+    .then(() => {
+      return res.status(200).send("토큰 삭제 완료");
+    });
 
   return res.status(400);
 };
