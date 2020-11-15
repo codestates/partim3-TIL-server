128647e17c982df37c28fede1c4e6bd2ae8413e2
sueryong <csr0604@hanmail.net>
2020-11-15 17:07:05 +0900

    [로그인 api 구현] 소셜로그인 및 토큰 전송방식 변경

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,7 +1,9 @@
 import login from "./login";
 import signup from "./signup";
+import social from "./social";
 
 export default {
   login,
   signup,
+  social,
 };
--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -7,91 +7,36 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default async (req: Request, res: Response) => {
-  const { email, password, nickname, oauthType, id } = req.body;
+  const { email, password } = req.body;
 
-  if (
-    oauthType === "kakao" ||
-    oauthType === "github" ||
-    oauthType === "naver" ||
-    oauthType === "google"
-  ) {
-    const user: any = await getRepository(User)
-      .createQueryBuilder("user")
-      .where("user.socialId= :socialId", { socialId: id })
-      .getOne();
+  const user = await getRepository(User)
+    .createQueryBuilder("user")
+    .where("user.email= :email", { email })
+    .getOne();
 
-    if (user === undefined) {
-      const token = await jwt.sign(
-        {
-          id,
-        },
-        `${process.env.TOKEN_SECRET}`
-      );
+  if (user === undefined) {
+    return res.status(401).send("회원 정보 없음");
+  } else if (user.email === email && user.password === password) {
+    const token: any = await jwt.sign(
+      {
+        email,
+      },
+      `${process.env.TOKEN_SECRET}`
+    );
 
-      await getConnection()
-        .createQueryBuilder()
-        .insert()
-        .into(User)
-        .values({
-          oauthType,
-          nickname,
-          socialId: id,
-        })
-        .execute();
+    await getConnection()
+      .createQueryBuilder()
+      .update(User)
+      .set({ token })
+      .where("email = :email", { email })
+      .execute();
 
-      res.setHeader("Authorizatioin", token);
-
-      return res.status(200).send("유저데이터 추가 및 토큰 생성완료");
-    } else if (user.socialId === id && user.oauthType === oauthType) {
-      const token = await jwt.sign(
-        {
-          id,
-        },
-        `${process.env.TOKEN_SECRET}`
-      );
-
-      await getConnection()
-        .createQueryBuilder()
-        .update(User)
-        .set({ token })
-        .where("socialId = :socialId", { socialId: id })
-        .execute();
-
-      res.setHeader("Authorizatioin", token);
-
-      return res.status(200).send("토큰 생성 완료");
-    } else {
-      return res
-        .status(409)
-        .send("아이디는 같은데 타입이 다른게 데이터 베이스에 있음");
-    }
-  } else {
-    const user = await getRepository(User)
-      .createQueryBuilder("user")
-      .where("user.email= :email", { email })
-      .getOne();
-
-    if (user === undefined) {
-      return res.status(401).send("회원 정보 없음");
-    } else if (user.email === email && user.password === password) {
-      const token: any = await jwt.sign(
-        {
-          email,
-        },
-        `${process.env.TOKEN_SECRET}`
-      );
-
-      await getConnection()
-        .createQueryBuilder()
-        .update(User)
-        .set({ token })
-        .where("email = :email", { email })
-        .execute();
-
-      res.setHeader("Authorizatioin", token);
-
-      return res.status(200).send("로그인 완료 및 토큰 생성 완료");
-    }
+    return res.status(200).json({
+      id: user.id,
+      nickname: user.nickname,
+      token,
+    });
   }
+
   return res.status(400);
 };
