f0c65c394fd7407ecd88bb153fd4bd912a7bdc53
sueryong <csr0604@hanmail.net>
2020-11-21 21:11:50 +0900

    [캘린더 api] day 수정....

--- src/controllers/calendar/day.ts
+++ src/controllers/calendar/day.ts
@@ -3,53 +3,56 @@ import { getRepository } from "typeorm";
 import { User } from "../../db/entities/User";
 
 export default async (req: Request, res: Response) => {
-  const { userId, date } = req.params;
+  const userId2: string = req.query.userId!;
+  const date: String = req.query.date;
 
-  const resultTodos: Array<any> = [];
-  const resultReviews: Array<any> = [];
+  console.log("데이트다", JSON.parse(date));
 
-  const todos = await getRepository(User)
-    .createQueryBuilder("user")
-    .leftJoinAndSelect("user.todos", "todo")
-    .where("user.id = :id", { id: userId })
-    .getMany()
-    .then((result) => {
-      if (result !== undefined) {
-        // tslint:disable-next-line: prefer-for-of
-        for (let i = 0; i < result[0].todos.length; i++) {
-          if (result[0].todos[i].scheduleTime === date) {
-            resultTodos.push(result[0].todos[i]);
-          }
-        }
-      }
-      console.log("todos ", result[0].todos);
-    })
-    .catch((error) => {
-      res.status(401).send(error);
-    });
+  const todos: Array<any> = [];
+  const reviews: Array<any> = [];
 
-  const reviews = await getRepository(User)
-    .createQueryBuilder("user")
-    .leftJoinAndSelect("user.reviews", "review")
-    .where("user.id = :id", { id: userId })
-    .getMany()
-    .then((result) => {
-      if (result !== undefined) {
-        // tslint:disable-next-line: prefer-for-of
-        for (let i = 0; i < result[0].reviews.length; i++) {
-          if (result[0].reviews[i].scheduleTime === date) {
-            resultReviews.push(result[0].reviews[i]);
-          }
-        }
-      }
-    })
-    .catch((error) => {
-      res.status(401).send(error);
-    });
+  // await getRepository(User)
+  //   .createQueryBuilder("user")
+  //   .leftJoinAndSelect("user.todos", "todo")
+  //   .where("user.id = :id", { id: puserId })
+  //   .getMany()
+  //   .then((result) => {
+  //     if (result !== undefined) {
+  //       // tslint:disable-next-line: prefer-for-of
+  //       for (let i = 0; i < result[0].todos.length; i++) {
+  //         if (result[0].todos[i].scheduleTime === date) {
+  //           todos.push(result[0].todos[i]);
+  //         }
+  //       }
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     res.status(401).send(error);
+  //   });
+
+  // await getRepository(User)
+  //   .createQueryBuilder("user")
+  //   .leftJoinAndSelect("user.reviews", "review")
+  //   .where("user.id = :id", { id: userId })
+  //   .getMany()
+  //   .then((result) => {
+  //     if (result !== undefined) {
+  //       // tslint:disable-next-line: prefer-for-of
+  //       for (let i = 0; i < result[0].reviews.length; i++) {
+  //         const currentDay = JSON.parse(result[0].reviews[i].scheduleTime);
+  //         if (currentDay.year === date) {
+  //           reviews.push(result[0].reviews[i]);
+  //         }
+  //       }
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     res.status(401).send(error);
+  //   });
 
   return res.status(200).json({
-    resultTodos,
-    resultReviews,
+    todos,
+    reviews,
   });
 
   return res.status(400);
