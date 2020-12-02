2354aeba6deef41069721ab25d2d07ac797c3bbe
sueryong <csr0604@hanmail.net>
2020-12-02 23:45:23 +0900

    [태그 가져오기 api 구현] 태그 가져오기 api 구현

---
+++ src/controllers/calendar/getTags.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../../db/entities/User';
+
+export default async (req: Request, res: Response) => {
+  const userId = Number(req.query.userId);
+
+  const _myTags = await getRepository(User)
+    .createQueryBuilder('user')
+    .leftJoinAndSelect('user.tags', 'tags')
+    .where('user.id= :id', { id: userId })
+    .getMany()
+    .catch((error) => {
+      res.status(401).send(error);
+    });
+
+  console.log(_myTags);
+  if (_myTags) {
+    if (_myTags[0]) {
+      return res.status(200).json({
+        myTags: _myTags[0].tags,
+      });
+    }
+  }
+
+  return res.status(400);
+};
