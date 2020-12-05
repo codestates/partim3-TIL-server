46b4aaac46b37fd88838159ca9cb7001c1d92128
sueryong <csr0604@hanmail.net>
2020-12-05 21:04:54 +0900

    [auth  api] 변수 수정

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -4,13 +4,13 @@ import { User } from '../../db/entities/User';
 import { IUser } from '../../types/IUser';
 
 export default async (req: Request, res: Response) => {
-  const { id } = req.body as IUser;
+  const { userId } = req.body as IUser;
 
   await getConnection()
     .createQueryBuilder()
     .update(User)
     .set({ token: undefined })
-    .where('id = :id', { id })
+    .where('id = :id', { id: userId })
     .execute()
     .then(() => {
       return res.status(200).send('로그아웃 성공');
