a3e96c7728b9f8e1a0ead254c2e65393c42b56ab
sueryong <csr0604@hanmail.net>
2020-12-09 20:40:45 +0900

    [day api] 코드 수정

--- src/controllers/calendarControllers/day.ts
+++ src/controllers/calendarControllers/day.ts
@@ -4,10 +4,24 @@ import { getRepository } from 'typeorm';
 import { Calendar } from '../../db/entities/Calendar';
 import { Todo } from '../../db/entities/Todo';
 import { Review } from '../../db/entities/Review';
+import { User } from '../../db/entities/User';
 
 export default async (req: Request, res: Response) => {
   const userId = Number(req.query.userId);
   const dateString = req.query.date as string;
+
+  const myCalendars = await getRepository(User)
+    .createQueryBuilder('calendar')
+    .leftJoinAndSelect('user.myCalendars', 'myCalendars')
+    .where('user.id= :id', { id: userId })
+    .getOne();
+
+  if (!myCalendars) {
+    return res.status(200).json({
+      myCalendars: [],
+      shareCalendars: [],
+    });
+  }
 
   const _myCalendars = await getRepository(Calendar)
     .createQueryBuilder('calendar')
