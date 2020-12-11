311ae239f16f8542af9ba1dd7c3169fc21e35462
sueryong <csr0604@hanmail.net>
2020-12-11 22:11:11 +0900

    [캘린더 수정 api ] join 수정

--- src/controllers/calendarControllers/calendar/updateCalendar.ts
+++ src/controllers/calendarControllers/calendar/updateCalendar.ts
@@ -8,20 +8,15 @@ export default async (req: Request, res: Response) => {
   const { userId, calendarId, name, color } = req.body as ICalendar;
 
   try {
-    const _calendarAuthority = await getRepository(User)
+    const _myCalendars = await getRepository(User)
       .createQueryBuilder('user')
-      .leftJoinAndSelect('user.userAuthorities', 'userAuthorities')
-      .where('user.id= :id', { id: userId })
-      // .andWhere('userAuthorities.calendarId= :calendarId', { calendarId })
+      .leftJoinAndSelect('user.myCalendars', 'myCalendars')
+      .where('user.id= :userId', { userId })
+      .andWhere('myCalendars.id = :calendarId', { calendarId })
       .getOne();
 
-    console.log(_calendarAuthority);
-    if (!_calendarAuthority) {
-      return res.status(401).send('사용가능한 캘린더 없음');
-    } else {
-      // if (!_calendarAuthority.myCalendarAuthorities[0].write) {
-      //   return res.status(401).send('캘린더 쓰기 권한 없음');
-      // }
+    if (!_myCalendars) {
+      return res.status(400).send('유저가 가지고 있지 않은 캘린더');
     }
   } catch (error) {
     return res.status(400).send(error);
@@ -35,7 +30,7 @@ export default async (req: Request, res: Response) => {
         name,
         color,
       })
-      .where('id = :id', { id: calendarId })
+      .where('id = :calendarId', { calendarId })
       .execute();
 
     return res.status(200).send('캘린더 수정 완료');
