f83f8f998b0f4cbf38fc86973b233d4fabb3eaa5
sueryong <csr0604@hanmail.net>
2020-12-12 15:19:35 +0900

    [review 삭제 api] 코드 리펙토링

---
+++ src/controllers/calendarControllers/review/deleteReview.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../../../db/entities/User';
+import { Review } from '../../../db/entities/Review';
+import { Calendar } from '../../../db/entities/Calendar';
+import { IReview } from '../../../types/IReview';
+import { UserCalendarAuthority } from '../../../db/entities/UserCalendarAuthority';
+
+export default async (req: Request, res: Response) => {
+  const { userId, calendarId, reviewId } = req.body as IReview;
+
+  try {
+    const _myCalendars = await getRepository(User)
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.myCalendars', 'myCalendars')
+      .where('user.id= :userId', { userId })
+      .andWhere('myCalendars.id = :calendarId', { calendarId })
+      .getOne();
+
+    if (!_myCalendars) {
+      return res
+        .status(400)
+        .send('유저 정보 없음 또는 가지고 있지 않은 캘린더');
+    }
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+
+  try {
+    const _myReview = await getRepository(Calendar)
+      .createQueryBuilder('calendar')
+      .leftJoinAndSelect('calendar.reviews', 'reviews')
+      .where('reviews.calendarId = :calendarId', { calendarId })
+      .andWhere('reviews.id = :reviewId', { reviewId })
+      .getOne();
+
+    if (!_myReview) {
+      return res
+        .status(400)
+        .send('캘린더 정보 없음 또는 가지고 있지 않은 REVIEW');
+    }
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+
+  try {
+    await getRepository(Review)
+      .createQueryBuilder('review')
+      .delete()
+      .where('review.id= :reviewId', { reviewId })
+      .execute();
+
+    return res.status(200).send('Review 삭제 완료');
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
