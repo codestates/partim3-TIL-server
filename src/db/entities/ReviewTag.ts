08ea17c3721c644ce2b0f4eff7d05927bc20867d
sueryong <csr0604@hanmail.net>
2020-12-12 22:03:22 +0900

    [테이블 수정]

--- src/db/entities/ReviewTag.ts
+++ src/db/entities/ReviewTag.ts
@@ -1,9 +1,12 @@
-import { Entity, BaseEntity, ManyToOne } from 'typeorm';
+import { Entity, BaseEntity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Review } from './Review';
 import { Tag } from './Tag';
 
 @Entity()
 export class ReviewTag extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
   @ManyToOne(() => Tag, (tag) => tag.reviewTags, {
     nullable: false,
     onDelete: 'CASCADE',
--- src/db/entities/TodoTag.ts
+++ src/db/entities/TodoTag.ts
@@ -1,9 +1,12 @@
-import { Entity, BaseEntity, ManyToOne } from 'typeorm';
+import { Entity, BaseEntity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Tag } from './Tag';
 import { Todo } from './Todo';
 
 @Entity()
 export class TodoTag extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
   @ManyToOne(() => Tag, (tag) => tag.todoTags, {
     onDelete: 'CASCADE',
     nullable: false,
