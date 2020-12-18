39f416323a41df219d85bfd236b18f8fc7dc6aaa
sueryong <csr0604@hanmail.net>
2020-12-19 02:22:38 +0900

    [todo 가져오기 api 구현] 사용자가 가지고 있는 todo만 가져오기(공유 캘린더 포함)

---
+++ src/controllers/calendarControllers/todo/getTodo.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../../../db/entities/User';
+import { Todo } from '../../../db/entities/Todo';
+
+export default async (req: Request, res: Response) => {
+  const userId = Number(req.query.userId);
+
+  try {
+    const _user = await getRepository(User)
+      .createQueryBuilder('user')
+      .where('user.id= :userId', { userId })
+      .getOne();
+
+    if (!_user) {
+      return res.status(400).send('유저 정보 없음');
+    }
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+
+  try {
+    const _calendarAthoritys = getRepository(User)
+      .createQueryBuilder('user')
+      .leftJoinAndSelect(
+        'user.userCalendarAuthorities',
+        'userCalendarAuthorities'
+      )
+      .leftJoinAndSelect(
+        'userCalendarAuthorities.calenderAuthority',
+        'calenderAuthority'
+      )
+      .leftJoinAndSelect('calenderAuthority.calendar', 'calendar')
+      .select('calendar.id')
+      .where('user.id = :userId', { userId });
+
+    const _todos = await getRepository(Todo)
+      .createQueryBuilder('todo')
+      .where('todo.calendarId IN (' + _calendarAthoritys.getQuery() + ')')
+      .setParameters(_calendarAthoritys.getParameters())
+      .getMany();
+
+    return res.status(200).json(_todos);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(error);
+  }
+};
