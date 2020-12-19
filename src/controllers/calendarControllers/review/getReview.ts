9e6b51a710b8c860ecde951cc167afba3658b132
sueryong <csr0604@hanmail.net>
2020-12-19 14:17:37 +0900

    [review 가져오기 api 구현]

---
+++ src/controllers/calendarControllers/review/getReview.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../../../db/entities/User';
+import { Review } from '../../../db/entities/Review';
+
+export default async (req: Request, res: Response) => {
+  const userId = Number(req.query.userId);
+
+  try {
+    const _user = await getRepository(User)
+      .createQueryBuilder('user')
+      .where('user.id= :userId', { userId })
+      .getOne();
+
+    if (!_user) {
+      return res.status(400).send('유저 정보 없음');
+    }
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+
+  try {
+    const _calendarAthoritys = getRepository(User)
+      .createQueryBuilder('user')
+      .leftJoinAndSelect(
+        'user.userCalendarAuthorities',
+        'userCalendarAuthorities'
+      )
+      .leftJoinAndSelect(
+        'userCalendarAuthorities.calenderAuthority',
+        'calenderAuthority'
+      )
+      .leftJoinAndSelect('calenderAuthority.calendar', 'calendar')
+      .select('calendar.id')
+      .where('user.id = :userId', { userId });
+
+    const _reviews = await getRepository(Review)
+      .createQueryBuilder('review')
+      .where('review.calendarId IN (' + _calendarAthoritys.getQuery() + ')')
+      .setParameters(_calendarAthoritys.getParameters())
+      .getMany();
+
+    return res.status(200).json({
+      reviews: _reviews,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(error);
+  }
+};
